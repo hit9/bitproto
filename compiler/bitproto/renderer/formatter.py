64ed9575f876ad72bc83a5fd33bee21d7222f046
hit9 <hit9@icloud.com>
2021-01-11 17:22:50 +0800

    bugfix of formatter format_definition_name

--- compiler/bitproto/renderer/formatter.py
+++ compiler/bitproto/renderer/formatter.py
@@ -12,11 +12,11 @@ from typing import Callable, Dict, List, Optional, Tuple
 from typing import Type as T
 from typing import TypeVar, Union, cast
 
-from bitproto._ast import (Alias, Array, Bool, BooleanConstant, Byte, Constant,
-                           Definition, Enum, EnumField, Int, Integer,
-                           IntegerConstant, Message, MessageField, Node, Proto,
-                           Scope, SingleType, StringConstant, Type, Uint,
-                           Value)
+from bitproto._ast import (Alias, Array, Bool, BooleanConstant, BoundScope,
+                           Byte, Constant, Definition, Enum, EnumField, Int,
+                           Integer, IntegerConstant, Message, MessageField,
+                           Node, Proto, Scope, SingleType, StringConstant,
+                           Type, Uint, Value)
 from bitproto.errors import InternalError
 from bitproto.utils import (final, keep_case, overridable, pascal_case,
                             snake_case, upper_case)
@@ -253,37 +253,43 @@ class Formatter:
             return 64
         return nbytes * 8
 
-    @final
-    def _format_definition_name(self, d: Definition) -> str:
-        """Formats the declaration name for given definition in target language.
-        Target languages may disallow nested declarations (such as structs, enums,
-        constants, typedefs).
-        Example output format: "Scope1_Scope2_{definition_name}".
-        """
-        if len(d.scope_stack) == 0:
-            return d.name  # Current bitproto.
-
+    def _get_definition_name(self, d: Definition) -> str:
+        """Get definition name, name defined in its scope or its original name."""
+        if len(d.scope_stack) <= 0:
+            return d.name
         scope = d.scope_stack[-1]
-        definition_name = scope.get_name_by_member(d) or d.name
+        return scope.get_name_by_member(d) or d.name
+
+    def _format_definition_name_inner_proto(self, d: Definition) -> str:
+        """Formats the declaration name for given definition in target language inner a
+        proto. Target languages may disallow nested declarations (such as structs, enums,
+        constants, typedefs), so we join the names of the definition's parent namespace
+        scopes with underscore. Example output format: "Scope1_Scope2_{definition_name}".
+        """
+        definition_name = self._get_definition_name(d)
 
         classes_ = self.scopes_with_namespace()
         namespaces = [scope for scope in d.scope_stack if isinstance(scope, classes_)]
 
-        if len(namespaces) == 0:
-            return definition_name  # Actually couldn't happen.
-        if len(namespaces) == 1 and isinstance(namespaces[0], Proto):
-            return definition_name
+        if len(namespaces) <= 1:
+            return definition_name  # Global definition.
 
-        namespace = namespaces[-1]
+        items: List[str] = [definition_name]
 
-        if isinstance(namespace, Proto):  # Cross proto
-            if not self.support_import_as_member():
-                return definition_name
-            items = [self._format_definition_name(namespace), definition_name]
-            return self.delimer_cross_proto().join(items)
-        else:
-            items = [self._format_definition_name(namespace), definition_name]
-            return self.delimer_inner_proto().join(items)
+        for namespace in namespaces[::-1]:  # Assuming its bound at last.
+            if isinstance(namespace, BoundScope):
+                items.insert(0, self._get_definition_name(namespace))
+            elif isinstance(namespace, Proto):
+                break
+        return self.delimer_inner_proto().join(items)
+
+    def format_definition_name_inner_proto(
+        self, d: Definition, class_: Optional[T[Definition]] = None
+    ) -> str:
+        """`_format_definition_name_inner_proto` with case style converting."""
+        class_ = class_ or d.__class__
+        name = self._format_definition_name_inner_proto(d)
+        return self.format_case_style(name, class_)
 
     @final
     def format_case_style(self, s: str, class_: T[Definition]) -> str:
@@ -315,10 +321,21 @@ class Formatter:
     def format_definition_name(
         self, d: Definition, class_: Optional[T[Definition]] = None
     ) -> str:
-        """The _format_definition_name with case converting."""
-        class_ = class_ or d.__class__
-        name = self._format_definition_name(d)
-        return self.format_case_style(name, class_)
+        """Formats the declaration name for given definition in target language.
+        Joins a with a dot delimer if given definition is a definition imported.
+        """
+        definition_name = self.format_definition_name_inner_proto(d, class_)
+        protos = [scope for scope in d.scope_stack if isinstance(scope, Proto)]
+
+        if len(protos) <= 1:
+            return definition_name
+
+        if not self.support_import_as_member():
+            return definition_name
+
+        proto = protos[-1]  # Last is the imported parent.
+        items = [self._get_definition_name(proto), definition_name]
+        return self.delimer_cross_proto().join(items)
 
     @final
     def format_enum_name(self, t: Enum) -> str:
