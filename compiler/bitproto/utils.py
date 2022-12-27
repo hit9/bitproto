0d371737292f3700630b37bb124c561920cb0f8f
hit9 <hit9@icloud.com>
2022-12-27 21:35:39 +0800

    Upgrade dev requirements

--- compiler/bitproto/utils.py
+++ compiler/bitproto/utils.py
@@ -126,7 +126,7 @@ class cached_property:
         ...
 
     @overload
-    def __get__(self, obj: I, cls: T[I]) -> O:
+    def __get__(self, obj: I, cls: T[I]) -> O:  # type: ignore
         ...
 
     def __get__(self, obj: Optional[I], cls: T[I]) -> Union["cached_property", O]:
