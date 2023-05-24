2219035e7c515f5297e3695741927ba226e29b83
hit9 <hit9@icloud.com>
2023-05-24 23:08:48 +0800

    bump 1.1.1

--- compiler/bitproto/__init__.py
+++ compiler/bitproto/__init__.py
@@ -4,9 +4,9 @@ bitproto
 
 Bit level data interchange format.
 
-:copyright: (c) 2020~2022 by Chao Wang <hit9@icloud.com>.
+:copyright: (c) 2020~2023 by Chao Wang <hit9@icloud.com>.
 
 """
 
-__version__ = "1.1.0"
+__version__ = "1.1.1"
 __description__ = "bit level data interchange format."
