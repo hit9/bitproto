c7b15cd52aca574367e101b920186206e3a164bf
hit9 <hit9@icloud.com>
2025-03-23 19:29:07 +0800

    v1.2.2: must use bitproto>=1.2.0

--- editors/language_server/setup.py
+++ editors/language_server/setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="bitproto-language-server",
-    version="1.2.1",
+    version="1.2.2",
     url="https://github.com/hit9/bitproto",
     author="Chao Wang",
     author_email="hit9@icloud.com",
-    long_description="""bitproto language server: https://github.com/hit9/bitproto""",
+    long_description="""bitproto language server: https://github.com/hit9/bitproto/tree/master/editors/language_server""",
     description="Language server for bitproto",
     packages=["bitproto_language_server"],
     include_package_data=True,
